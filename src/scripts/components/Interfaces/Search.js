bd660c31e026426afae9bf9650fae85c22c5e089
Florian Fries <fries.florian@gmail.com>
2015-06-07 22:55:42 +0200

    Add search mode (breaks current compatibility with the yourand server)

--- src/scripts/components/Interfaces/Search.js
+++ src/scripts/components/Interfaces/Search.js
@@ -2,16 +2,32 @@
 
 import React from 'react/addons';
 import SearchBar from '../UI/SearchBar';
+import YoutubePlayer from '../Video/YoutubePlayer';
 
-let InterfaceSearch = React.createClass({
+export default class InterfaceSearch extends React.Component {
+    static contextTypes = {
+        videostore: React.PropTypes.object.isRequired
+    };
+
+    componentWillMount() {
+        this.unwatchVideostore = this.context.videostore.watch(this.forceUpdate.bind(this));
+    };
+
+    componentWillUnmount() { this.unwatchVideostore(); }
+
     render() {
+
+        const { videostore } = this.context;
+
+        const videos = videostore.getVideos();
 
         return (
             <div>
                 <SearchBar />
+                {videos.length > 0 && (
+                    <YoutubePlayer video={videos[0]} />
+                )}
             </div>
         );
     }
-});
-
-module.exports = InterfaceSearch;
+}
--- src/scripts/components/UI/SearchBar.js
+++ src/scripts/components/UI/SearchBar.js
@@ -2,23 +2,40 @@
 
 import React from 'react/addons';
 
-let SearchBar = React.createClass({
+export default class SearchBar extends React.Component {
 
-    getInitialState() { return { value: '' }; },
+    static contextTypes = {
+        videoactions: React.PropTypes.object.isRequired
+    };
+
+    constructor() {
+        super();
+        this.state = { value: '' };
+    }
 
     render() {
 
-        const handleChange = () => {
-            /*this.props.flux.getActions('todos').serverSearchChannels(e.target.value);
-            this.setState({value: e.target.value});*/
+        const { videoactions } = this.context;
+
+        const handleChange = (e) => {
+            this.setState({value: e.target.value});
+            if(this.state.value.trim() === '') { return; }
+            videoactions.fetchVideosBySearch(e.target.value);
+        };
+
+        const handleClick = () => {
+            videoactions.fetchVideosBySearch(this.value.state);
         };
 
         return (
-            <form action="#">
-                <input type="text" className="searchbar" placeholder="Search for something..." autoComplete="off" value={this.state.value} onChange={handleChange} />
-            </form>
+            <div>
+                <form action="#">
+                    <input type="text" className="searchbar" placeholder="Search for something..." autoComplete="off" value={this.state.value} onChange={handleChange} />
+                </form>
+                <div className="channel-list">
+                    <div className="channel-item" onClick={handleClick}>Search for <strong>{this.state.value}</strong></div>
+                </div>
+            </div>
         );
     }
-});
-
-module.exports = SearchBar;
+}
